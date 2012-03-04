51db4cb076f65e6953f4e271ad57e72ee9d52a3e
Thomas Cowling <tcowling@thoughtworks.com>
2012-03-04 12:17:43 +0530

    TDD:1 Complete

---
+++ src/SavingsAccount.java
@@ -0,0 +1,15 @@
+public class SavingsAccount {
+    private int balance = 0;
+    
+    public void deposit(int amount){
+        balance += amount;
+    }
+    
+    public int balance(){
+        return balance;
+    }
+
+    public void withdraw(int amount) {
+        balance -= amount;
+    }
+}
---
+++ test/SavingsAccountTest.java
@@ -0,0 +1,23 @@
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+public class SavingsAccountTest {
+
+    @org.junit.Test
+    public void depositAndWithdrawal() {
+        SavingsAccount account = new SavingsAccount();
+        account.deposit(100);
+        assertThat(account.balance(), equalTo(100));
+        account.withdraw(50);
+        assertThat(account.balance(), equalTo(50));
+    }
+
+    @Test
+    public void negativeBalanceWorksJustFine() {
+        SavingsAccount account = new SavingsAccount();
+        account.withdraw(75);
+        assertThat(-75, equalTo(account.balance()));
+    }
+}
